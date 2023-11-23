2d33736bf16c612e61f35c000a4298913601cef1
Oak <mickyodum@gmail.com>
2023-11-23 23:05:20 +0100

    event listener intro

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-function save(){
-    console.log("Button Clicked!")
-}
+// function save(){
+//     console.log("Button Clicked!")
+// }
+
+let inputBtn = document.getElementById("input-btn")
+
+inputBtn.addEventListener("click", function(){
+    console.log("Button clicked from Event Listener")
+})
