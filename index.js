fa1b96d1145b5f6a78abd18f519c9e062f300c86
Oak <mickyodum@gmail.com>
2023-11-28 18:29:13 +0100

    template strings

--- index.js
+++ index.js
@@ -16,8 +16,13 @@ function renderLeads(){
     let listItems = ""
 
     for (let i = 0; i < myLeads.length; i++){
-        listItems += "<li>" + myLeads[i] + "</li>" 
-
+        listItems += `
+            <li>
+                <a href='${myLeads[i]}' target='_blank'>
+                    ${myLeads[i]}
+                </a>
+            </li>`
+        console.log(listItems)
         
     }
     ulEl.innerHTML = listItems
